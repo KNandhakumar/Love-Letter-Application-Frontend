56a2007d63cfa31634da7250a8ddad06f319018a
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-30 17:13:01 +0530

    update : created send letter service

--- src/app/letter.service.ts
+++ src/app/letter.service.ts
@@ -1,9 +1,18 @@
+import { Letter } from './letter.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LetterService {
+  constructor(private http:HttpClient) { }
+  // api url
+  private apiUrl = 'http://localhost:8080/api/letters';
 
-  constructor() { }
+  // send letter
+  sendLetter(letter:Letter):Observable<Letter>{
+    return this.http.post<Letter>(this.apiUrl,letter);
+  }
 }
