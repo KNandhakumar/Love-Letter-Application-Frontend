9b6250b8e1ca56488df07e6cfd0ab01ca2077eb2
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-30 14:32:59 +0530

    update : letter component. added form for sending letters

--- src/app/pages/letter/letter.component.ts
+++ src/app/pages/letter/letter.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-letter',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './letter.component.html',
   styleUrl: './letter.component.css'
 })
